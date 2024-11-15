446b3827a1dfe5d38aece4f88d9e1521ea0353b5
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2024-11-15 09:45:01 -0500

    refined initial setup
    more secure and thorough

--- inital_setup.sh
+++ inital_setup.sh
@@ -5,13 +5,15 @@
 echo "Updating OS and installing nginx with rtmp support"
 apt update > /dev/null
 apt dist-upgrade -y > /dev/null
-apt install libnginx-mod-rtmp -y > /dev/null
+apt install libnginx-mod-rtmp gunzip -y > /dev/null
 
 cat >/etc/nginx/rtmp.conf <<EOL
 rtmp {
   server {
     listen 1935;
     chunk_size 4096;
+    allow publish 127.0.0.1;
+    deny publish all;
     application live {
       live on;
       record off;
@@ -21,10 +23,37 @@ rtmp {
 EOL
 
 sed -e '/include /etc/nginx/modules-enabled/*.conf;/a\'$'\n''include /etc/nginx/rtmp.conf;' /etc/nginx/nginx.conf
-systemctl daemon-reload
-systemctl restart nginx
 
-echo "Activated RTMP on port 1935."
+echo "Activated RTMP on port 1935 for nginx."
+
+cat >/etc/nginx/sites-available/rtmp <<EOL
+server {
+    listen 8080;
+    server_name  localhost;
+
+    # rtmp stat
+    location /stat {
+        rtmp_stat all;
+        rtmp_stat_stylesheet stat.xsl;
+    }
+    location /stat.xsl {
+        root /var/www/html/rtmp;
+    }
+
+    # rtmp control
+    location /control {
+        rtmp_control all;
+    }
+}
+EOL
+
+mkdir /var/www/html/rtmp
+gunzip -c /usr/share/doc/libnginx-mod-rtmp/examples/stat.xsl.gz > /var/www/html/rtmp/stat.xsl
+ln -s /etc/nginx/sites-available/rtmp /etc/nginx/sites-enabled/rtmp
+
+systemctl reload nginx
+
+echo "Activated RTMP stats on :8080/stat"
 
 read -p "Where is the webrun.sh script going to live? Please use the full path" script_loc
 
@@ -50,9 +79,9 @@ read -p "Where is the label going to live?" label_loc
 
 read -p "Where is the logo going to live?" logo_loc
 
-sed s/"POC Camera"/$cam_name $script_loc
-sed s/"label.txt"/$label_loc $script_loc
-sed s/"logo.png"/$logo_loc $script_loc
+sed "0/POC Camera/s//$cam_name/" $script_loc
+sed "0/label.txt/s//$label_loc/" $script_loc
+sed "0/logo.png/s//$logo_loc/" $script_loc
 
 systemctl enable webstream
 
